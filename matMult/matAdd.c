0742837e7c6304d40449a8b5f38fc7c701eff0e8
Tessa Tapmongkol <ttapmong@calpoly.edu>
2021-06-03 11:26:46 -0700

    Added files

---
+++ matMult/matAdd.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void matadd (int **C, int **A, int **B, int height, int width) {
+   for (int i = 0; i < height; ++i) {
+      for (int j = 0; j < width; ++j) {
+         for (int k = 0; k < width; ++k) {
+            C[i][j] += A[i][k] * B[k][j];
+         }
+      }
+   }
+}
+
+void printMatrix(int **C, int height, int width) {
+
+   for (int i = 0; i < height; ++i) {
+      for (int j = 0; j < width; ++j) {
+         printf("%d  ", C[i][j]);
+         if (j == width - 1)
+            printf("\n");
+      }
+   }
+}
+
+int main() {
+    int **A;
+    int **B;
+    int **C;
+
+    int height = 10;
+    int width = 10;
+
+    A = (int**)malloc(sizeof(int*)*height);
+    B = (int**)malloc(sizeof(int*)*height);
+    C = (int**)malloc(sizeof(int*)*height);
+
+    for(int i = 0; i < width; i++) {
+        A[i] = malloc(sizeof(int)*width);
+        B[i] = malloc(sizeof(int)*width);
+        C[i] = malloc(sizeof(int)*width);
+    }
+
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            A[i][j] = rand() % 2;
+            B[i][j] = rand() % 2;
+        }
+    }
+
+    matadd(C, A, B, height, width); 
+    printMatrix(C, height, width);
+}
